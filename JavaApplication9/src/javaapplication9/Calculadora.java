e25a90a2d28fff5f8175cc15aecb09133ea4fe8c
v-pelaez <victor.pelaez.perez@gmail.com>
2025-05-01 16:53:49 +0200

    updt Metodo Sumar

---
+++ JavaApplication9/src/javaapplication9/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author KHVREM
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
